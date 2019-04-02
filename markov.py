cdb1d6ccdb7fdafaca0bf458599058be69837b31
sortai <32301806+sortai@users.noreply.github.com>
2019-04-02 16:49:57 +0200

    aesthetics

--- markov.py
+++ markov.py
@@ -1,11 +1,11 @@
 class markov:
-    def __init__(self, base = None, blsize = 1):
+    def __init__(self, base = None, plen = 1):
         if base is None:
             base = {0:{0:1}} #default markov chain
         if type(base) == dict:
             self.pats = dict(base)
         else:
-            self.pats=dict()
-            for i in range(len(base)-blsize+1):
-                try: self.pats[base[i:i+blsize]]+=1
-                except KeyError: self.pats[base[i:i+blsize]]=1
+            self.pats = dict()
+            for i in range(len(base)-plen+1):
+                try: self.pats[base[i:i+plen]] += 1
+                except KeyError: self.pats[base[i:i+plen]] = 1
