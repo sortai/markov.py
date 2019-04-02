8ea4122cb2686a5a0c58386d97a05f353f986cf0
sortai <32301806+sortai@users.noreply.github.com>
2019-04-02 16:32:26 +0200

    added support to looping series

--- bldup.py
+++ bldup.py
@@ -1,5 +1,8 @@
-def bldup(x, s=1):
+def bldup(x, s=1, loop=True):
     l = len(x)
     for i in range(l):
-        if i>=l-s: break
-        yield (x[i:i+s],x[i+1:i+s+1])
+        if i>=l-s:
+            if loop:
+                yield (x[i:i+s]+x[0:i+s-l],x[i+1:i+s+1]+x[0:i+s-l+1])
+            else: break
+        else: yield (x[i:i+s],x[i+1:i+s+1])
