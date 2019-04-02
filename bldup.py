33e0419431915badf30a0c4395891137144ab394
sortai <32301806+sortai@users.noreply.github.com>
2019-04-02 16:34:39 +0200

    Delete bldup.py

--- bldup.py
+++
@@ -1,8 +0,0 @@
-def bldup(x, s=1, loop=True):
-    l = len(x)
-    for i in range(l):
-        if i>=l-s:
-            if loop:
-                yield (x[i:i+s]+x[0:i+s-l],x[i+1:i+s+1]+x[0:i+s-l+1])
-            else: break
-        else: yield (x[i:i+s],x[i+1:i+s+1])
