3fd78e9eb5365c87400b7f5971e38fa379ecf293
sortai <32301806+sortai@users.noreply.github.com>
2019-04-02 15:59:56 +0200

    Update markov.py

--- markov.py
+++ markov.py
@@ -1,11 +1,10 @@
 
 class markov:
-    def __init__(self, base=None):
+    def __init__(self, base=None, blsize=1):
         if base is None:
             base = {0:{0:1}} #default markov chain
         if type(base) == dict:
             self.graph = dict(base)
         else:
             for x in base: #iterability test
-                break
-        
+                pass
