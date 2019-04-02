16e860ba2487aae9333d1a37e5e9ed65ca87123d
sortai <32301806+sortai@users.noreply.github.com>
2019-04-02 16:40:40 +0200

    Update markov.py

--- markov.py
+++ markov.py
@@ -1,19 +1,11 @@
-from bldup import bldup
-
 class markov:
-    def __init__(self, base = None, blsize = 1, baseloop = True):
+    def __init__(self, base = None, blsize = 1):
         if base is None:
             base = {0:{0:1}} #default markov chain
         if type(base) == dict:
-            self.graph = dict(base)
+            self.pats = dict(base)
         else:
-            self.graph=dict()
-            for t0, t1 in bldup(base, blsize, baseloop): #iterability test (indirect)
-                try: self.graph[t0][t1]+=1
-                except KeyError:
-                    try: self.graph[t0][t1]=1
-                    except KeyError:
-                        self.graph[t0]={t1:1}
-        self.normalize()
-    def normalize(self):
-        pass
+            self.pats=dict()
+            for i in range(len(base)-blsize+1):
+                try: self.pats[base[i:i+blsize]]+=1
+                except KeyError: self.pats[base[i:i+blsize]]=1
